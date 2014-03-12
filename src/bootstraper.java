b970a1240bd905c940561db4c995da6f23c83855
crushack <psilviu94@gmail.com>
2014-03-12 22:49:40 +0200

    added game mode classes

---
+++ src/bootstraper.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class bootstraper {
+
+	BufferedReader br;
+	
+	public bootstraper () {
+		br = new BufferedReader( new InputStreamReader(System.in));
+	}
+	
+	public void run() {
+		
+	}
+	
+}
---
+++ src/game.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+
+
+public class game {
+
+	BufferedReader br;
+	
+	public game(BufferedReader br) {
+		this.br = br;
+	}
+	
+	public void run() {
+		
+	}
+}
