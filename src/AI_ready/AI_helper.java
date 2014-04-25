ece39da62dcd4bfbce6cb59685ce24f1a857aa53
crushack <psilviu94@gmail.com>
2014-04-25 11:32:21 +0300

    Ai helper methods

---
+++ src/AI_ready/AI_helper.java
@@ -0,0 +1,57 @@
+package AI_ready;
+
+public class AI_helper {
+
+	public static int center_control( String [] map, boolean[][] moved ) {
+		// TODO
+		return 0;
+	}
+	
+	public static int focal_center_control( String [] map, boolean [][] moved ) {
+		
+		// TODO
+		return 0;
+	}
+	
+	public static int development_order( String [] map, boolean [][] moved ) {
+		
+		// TODO
+		return 0;
+	}
+	
+	public static int underdevelopment( String [] map, boolean [][] moved ) {
+		
+		// TODO
+		return 0;
+	}
+	
+	public static int phalangian_development( String [] map, boolean [][] moved) {
+		
+		// TODO
+		return 0;
+	}
+	
+	public static int losing_castlling_righs( String [] map, boolean [][] moved ) {
+		
+		// TODO
+		return 0;
+	}
+	
+	public static int temporary_prevention_of_castlinf( String [] map, boolean [][] moved ) {
+		
+		// TODO
+		return 0;
+	}
+	
+	public static int queen_excange( String [] map, boolean [][] moved ) {
+		
+		// TODO
+		return 0;
+	}
+	
+	public static int pins( String [] map, boolean [][] moved ) {
+		
+		// TODO
+		return 0;
+	}
+}
